38e39d9de02f7e8c178d776f8d069468e5d6b9f6
devilune <devilune@wizlog.io>
2017-09-18 00:14:44 +0900

    Add routing and sample page

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,31 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 import './index.css';
 
+interface AppState {
+}
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+
+const store = createStore<AppState>(
+  combineReducers({
+    router: routerReducer,
+  }),
+  applyMiddleware(middleware)
+);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App/>
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
